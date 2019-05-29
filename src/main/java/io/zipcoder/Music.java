9fcc48452b288b7ca976f60513968d1f952978ab
Robert Rieger <robertrieger@zipcoders-MacBook-Pro-96.local>
2019-05-29 19:34:08 -0400

    Completed Playlist Lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,39 @@
 package io.zipcoder;
 
+import javax.swing.text.MutableAttributeSet;
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
+    int result;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
-    }
+    public Integer selection(Integer startIndex, String selection) {
+
+        int selectionIndex = 0;
+
+        for (int i = 0; i < playList.length; i++)
+            if (playList[i] == selection) {
+                selectionIndex = i;
+            }
+
+         int up = Math.abs(selectionIndex - startIndex);
+         int wrap = playList.length - up;
+
+         if(up > wrap) {
+             return wrap;
+         } else {
+            return up;
+         }
+
+    }//end of method
 }
+
+
+
+//   1 2 3 4 5 6
+//     i       s
